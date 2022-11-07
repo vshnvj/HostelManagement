04a6328599528622368806fa6d73bdd0fa3d8a43
vshnvj <vishjadhav318@gmail.com>
2022-11-07 09:12:55 +0530

    track rent admin side1st commit

--- HostelManagement/Models/ViewModel.cs
+++ HostelManagement/Models/ViewModel.cs
@@ -12,9 +12,9 @@ namespace HostelManagement.Models
         public int User_id;
         public string Name;
         public string Mobile;
-        public string Rent;
+        public Nullable<int> Rent;
 
-        [DataType(DataType.Date), DisplayFormat(DataFormatString = @"{0:dd\/MM\/yyyy }")]
+        //[DataType(DataType.Date), DisplayFormat(DataFormatString = @"{0:dd\/MM\/yyyy }")]
         public Nullable<System.DateTime> Date_of_payment;
        public Nullable<int>  Amount;
     }
